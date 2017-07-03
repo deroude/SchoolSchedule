773e017913b85b0e80cc3ffe55ae07674efaa909
Valentin Raduti <valentin.raduti@computaris.com>
2017-07-03 15:03:13 +0300

    heroku attempt #4

--- bs-config.js
+++ bs-config.js
@@ -2,6 +2,6 @@
 
 module.exports={
   "port": process.env.PORT||5050,
-  "files": ["../dist/**/*.{html,htm,css,js}"],
+  "files": ["/dist/**/*.{html,htm,css,js}"],
   "server": { "baseDir": "." }
 }
